0c055d01f27b0c31abf763063d623999ad89afca
Serbull <4weg4@mail.ru>
2026-02-18 15:20:03 +0800

    Update SceneContext.cs

--- Samples/Scripts/SceneContext.cs
+++ Samples/Scripts/SceneContext.cs
@@ -18,7 +18,7 @@ namespace Serbull.GameAssets.Pets.Samples
 
             var lang = "ru";
 
-            _sgaInstaller.Init(null, null);
+            _sgaInstaller.Init(null, null, false);
             _petInstaller.Init(Currency, SaveData.Pets, lang);
         }
     }
